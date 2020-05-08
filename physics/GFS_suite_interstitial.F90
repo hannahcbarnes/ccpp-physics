3280060a8d5ff533bc5473ba51177c64b93712cf
hannah barnes <hannah.barnes@noaa.gov>
2020-05-08 08:59:36 -0600

    Number concentration bug fix

--- physics/GFS_suite_interstitial.F90
+++ physics/GFS_suite_interstitial.F90
@@ -725,7 +725,7 @@
                  qv_mp(i,k) = spechum(i,k)/(1.0_kind_phys-spechum(i,k))
                  if (ntlnc>0) then
                    !> - Convert moist mixing ratio to dry mixing ratio
-                   qc_mp(i,k) = save_qc(i,k)/(1.0_kind_phys-spechum(i,k))
+                   qc_mp(i,k) = (clw(i,k,2)-save_qc(i,k))/(1.0_kind_phys-spechum(i,k))
                    !> - Convert number concentration from moist to dry
                    nc_mp(i,k) = gq0(i,k,ntlnc)/(1.0_kind_phys-spechum(i,k))
                    nc_mp(i,k) = nc_mp(i,k) + max(0.0, make_DropletNumber(qc_mp(i,k) * rho_dryair(i,k), nwfa(i,k)) * (1.0/rho_dryair(i,k)))
@@ -734,7 +734,7 @@
                  endif
                  if (ntinc>0) then
                    !> - Convert moist mixing ratio to dry mixing ratio
-                   qi_mp(i,k) = save_qi(i,k)/(1.0_kind_phys-spechum(i,k))
+                   qi_mp(i,k) = (clw(i,k,1)-save_qi(i,k))/(1.0_kind_phys-spechum(i,k))
                    !> - Convert number concentration from moist to dry
                    ni_mp(i,k) = gq0(i,k,ntinc)/(1.0_kind_phys-spechum(i,k)) 
                    ni_mp(i,k) = ni_mp(i,k) + max(0.0, make_IceNumber(qi_mp(i,k) * rho_dryair(i,k), save_tcp(i,k)) * (1.0/rho_dryair(i,k)))
